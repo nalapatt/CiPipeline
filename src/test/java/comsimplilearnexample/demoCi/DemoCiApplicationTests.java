83c855b3c3c284a746ee4b7410e33075e81e82c4
Ubuntu <kpisipati@jenkins-tutorial-vm.jb2umvu10rlujn2uk2jo4hat1a.bx.internal.cloudapp.net>
2020-11-21 18:03:01 +0000

    first commit of pom.xml files

---
+++ src/test/java/comsimplilearnexample/demoCi/DemoCiApplicationTests.java
@@ -0,0 +1,13 @@
+package comsimplilearnexample.demoCi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
